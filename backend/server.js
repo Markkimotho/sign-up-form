502b80949a195fb7f23cb2b05c0841498ec85064
Markkimotho <martinozas@gmail.com>
2024-09-17 23:18:58 +0300

    Set up root package.json and Procfile for Heroku

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// Serve static files from the 'frontend' directory
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+// Serve index.html as the default route
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
 app.listen(port, () => {
