f18ab7bc1f8cd6a35649a5f5661be304ad345dd3
Markkimotho <martinozas@gmail.com>
2024-09-17 23:22:12 +0300

    Set up route to serve signup and login pages

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, '../frontend')));
 
 // Serve index.html as the default route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/signup.html'));
 });
 
 app.listen(port, () => {
